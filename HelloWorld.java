2a7c6aa143dd4c3e4e1d956144e129ce207d9d8a
@Giddyspurz <iamroot2005@protonmail.com>
2022-05-26 19:43:51 +0300

    Add files via upload

---
+++ HelloWorld.java
@@ -0,0 +1,10 @@
+//First Tutorial Java
+//Giddyspurz
+
+public class HelloWorld{
+
+	public static void main(String[] args) {
+		//Prints "Hello, World " to the terminal Window.
+		System.out.println("Hello, Gad!");
+	}
+}
---
+++ Input.java
@@ -0,0 +1,17 @@
+//Giddyspurz
+//Scanner Inputs from Users
+
+import java.util.Scanner;
+
+public class Main {
+	public static void main (String[] args) {
+
+	Scanner sc = new Scanner(System.in);
+	String scanned = sc.next();
+	
+	System.out.println(scanned);
+	}
+}
+
+
+
